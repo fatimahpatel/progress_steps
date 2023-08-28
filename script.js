d94f3d4cc5d0a1a1746ad0a32f69c3853793e79c
Fatimah Patel <fatimahpatel10@gmail.com>
2023-08-28 20:24:31 +0100

    if statement so when the current position is 1 (index 0), the prev button is disabled

--- script.js
+++ script.js
@@ -40,4 +40,8 @@ function update(){
 
     const actives = document.querySelectorAll('.active')
     progress.style.width = ((actives.length - 1)/(circles.length - 1)) * 100 + '%';
+
+    if(currentActive === 1){
+        prev.disabled = true; 
+    }
 }
