0af3197e60512519f3a081f4440cb960d921e7a0
Fatimah Patel <fatimahpatel10@gmail.com>
2023-08-28 18:05:39 +0100

    new function where next button is clicked, it increments currentActive by one

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('previous');
 const next = document.getElementById('next');
 const circle = document.querySelectorAll('.circle');
+
+// this represents whichever circle is active
+let currentActive = 1;
+
+next.addEventListener('click', ()=>{
+    currentActive++;
+    console.log(currentActive)
+})
