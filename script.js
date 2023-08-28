ea891adce839e13cc0531ba157eb11e4c2a63762
Fatimah Patel <fatimahpatel10@gmail.com>
2023-08-28 18:00:37 +0100

    new variables

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const progress = document.getElementById('progress');
+const prev = document.getElementById('previous');
+const next = document.getElementById('next');
+const circle = document.querySelectorAll('.circle');
